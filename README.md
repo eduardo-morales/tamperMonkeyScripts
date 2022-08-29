4f12d8867fc361a0329ce048c37d1a1c02641923
profesor <carlos@gmail.com>
2022-08-29 10:05:36 -0400

    se agrega script para abc color

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# tamperMonkeyScripts
+# tamperMonkeyScripts by @moralotto
+# userscripts
+
+Repo de scripts personales.  Para installar cualquiera de ellos:
+
+- Instalar [Tampermonkey para Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)
+or [Tampermonkey [para Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/).
+
+- Seleccionar un script de la lista, luego click en **Install this script**, luego **Install**:
+
+  - [login ABC Color](https://greasyfork.org/en/scripts/22551-quip): eliminar el popup de login de ABC color
---
+++ banner_abc.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Quitar login modal de ABC color
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Quitar el odioso modal de ABC color
+// @author       Eduardo Morales https://twitter.com/moralotto
+// @match        https://www.abc.com.py/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @require http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $(window).on('load', function() {
+       var body = document.getElementsByClassName("tp-modal-open")[0];
+        if (body !== undefined)
+        {
+            body.classList.remove('tp-modal-open');
+        }
+        $( ".tp-backdrop" ).remove();
+        $( ".tp-modal" ).remove();
+   })
+})();
