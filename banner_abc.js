f3f7a2a75468a1ab8e104a6039dcb66215cdad70
eduardo-morales <eduardomoralesf@gmail.com>
2024-07-13 16:30:42 -0400

    Update banner_abc.js

--- banner_abc.js
+++ banner_abc.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Quitar login modal de ABC color abc.com.py
+// @name         Quitar login modal de ABC color
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Quitar el odioso modal de ABC color abc.com.py
-// @author       Eduardo Morales https://twitter.com/moralotto
+// @description  Quitar el odioso modal de ABC color
+// @author       Eduardo Morales @moralotto
 // @match        https://www.abc.com.py/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        none
@@ -12,13 +12,30 @@
 
 (function() {
     'use strict';
-    $(window).on('load', function() {
-       var body = document.getElementsByClassName("tp-modal-open")[0];
-        if (body !== undefined)
-        {
-            body.classList.remove('tp-modal-open');
-        }
-        $( ".tp-backdrop" ).remove();
-        $( ".tp-modal" ).remove();
-   })
+
+    // Función para eliminar los elementos y la clase después de que la página se haya cargado completamente
+    function removeElementsAndClass() {
+        // Eliminar elementos con la clase .tp-backdrop
+        var backdrops = document.querySelectorAll('.tp-backdrop');
+        backdrops.forEach(function(backdrop) {
+            backdrop.remove();
+        });
+
+        // Eliminar elementos con la clase .tp-modal
+        var modals = document.querySelectorAll('.tp-modal');
+        modals.forEach(function(modal) {
+            modal.remove();
+        });
+
+        // Eliminar la clase .tp-modal-open de los elementos que la contengan
+        var modalOpenElements = document.querySelectorAll('.tp-modal-open');
+        modalOpenElements.forEach(function(element) {
+            element.classList.remove('tp-modal-open');
+        });
+    }
+
+    // Ejecutar la función después de que la página se haya cargado completamente
+    //window.addEventListener('load', removeElementsAndClass);
+    // Ejecutar la función cada segundo
+    setInterval(removeElementsAndClass, 1000);
 })();
